daac7a838c461745cdca65a3b4f192808986afae
Martin Stumpf <martin.h.stumpf@gmail.com>
2016-11-15 17:22:29 +0100

    updated TODO

--- generate.sh
+++ generate.sh
@@ -9,4 +9,4 @@ mkdir ${BUILD_PATH}
 
 cd ${BUILD_PATH}
 
-CXX=clang++ CC=clang cmake -DLEMON_DIR="/usr/lib/cmake" -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ${SCRIPTPATH}
+CXX=clang++ CC=clang cmake -DLEMON_DIR="$SCRIPTPATH/../liblemon/install/share/lemon/cmake" -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ${SCRIPTPATH}
