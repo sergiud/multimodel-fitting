3e72bb77c4c1afd94d4218bf6a3f985da2ecc0aa
Martin Stumpf <martin.h.stumpf@gmail.com>
2017-01-31 12:56:12 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ mkdir ${BUILD_PATH}
 cd ${BUILD_PATH}
 
 #CXX=clang++ CC=clang cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ${SCRIPTPATH}
-CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_INSTALL_PREFIX=$HOME/mmf_install -DCMAKE_BUILD_TYPE=Release ${SCRIPTPATH}
+CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_CXX_FLAGS="-Ofast -march=native" -DCMAKE_INSTALL_PREFIX=$HOME/mmf_install -DCMAKE_BUILD_TYPE=Release ${SCRIPTPATH}
+#CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_INSTALL_PREFIX=$HOME/mmf_install -DCMAKE_BUILD_TYPE=Release ${SCRIPTPATH}
 #CXX=g++ CC=gcc cmake -DCMAKE_INSTALL_PREFIX=$HOME/mmf_install -DCMAKE_BUILD_TYPE=Release ${SCRIPTPATH}
+#CXX=g++ CC=gcc cmake -DCMAKE_INSTALL_PREFIX=$HOME/mmf_install -DCMAKE_CXX_FLAGS="-Ofast -march=native" -DCMAKE_BUILD_TYPE=Release ${SCRIPTPATH}
 
 make
